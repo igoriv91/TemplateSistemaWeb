df1dece4407d31f3d0764abf5d40fa7f5727c097
igor <igorvitor.iv@gmail.com>
2024-04-08 08:41:35 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import Rotas from "./routes";
 
 function App() {
-
   return (
     <>
-    
+      <Rotas />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
